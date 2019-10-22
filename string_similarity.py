424110c8eebbbb0ad9fde7e825605fe847150d9e
Abhiram Bharadwaj <ab8136@g.rit.edu>
2019-10-21 20:48:24 -0400

    Solved string similarity

---
+++ string_similarity.py
@@ -0,0 +1,22 @@
+"""
+Author: Abhiram Ravi Bharadwaj
+Source: HackerRank
+Problem: https://www.hackerrank.com/challenges/string-similarity/problem
+"""
+
+def main():
+	s = 'ababaa'
+	z = [0] * len(s)
+	l = r = -1
+	for i in range(1, len(s)):
+		if (i <= r):
+			z[i] = min(r - i + 1, z[i - l])
+		while (i + z[i] < len(s) and s[z[i]] == s[i + z[i]]):
+			z[i] += 1
+		if (i + z[i] - 1 > r):
+			l = i
+			r = i + z[i] - 1
+	return sum(z) + len(s)
+
+if __name__ == '__main__':
+	main()
